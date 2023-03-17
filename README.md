64b42c11c2593b4faf3982e7558327a4e9ef484b
Aidan Pinard <aidan@aidanpinard.co>
2023-03-16 20:49:44 -0400

    Moved to a nix template
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # template-rs-nix
 
 Template for Rust on NixOS.
+
+TODO:
+
+- [ ] Add treefmt to template for formatting.
--- flake.nix
+++ flake.nix
@@ -1,39 +1,14 @@
 {
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+  description = "My Rust Flake Template";
+
+  outputs = { self }: {
+    templates = {
+      app = {
+        path = ./app;
+        description = "Flake for a Rust App using Naersk";
+      };
+    };
+
+    defaultTemplate = self.templates.app;
   };
-
-  outputs = { self, flake-utils, naersk, nixpkgs, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-
-        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-        inherit (cargoToml.package) name;
-        inherit (cargoToml.package) version;
-
-        naersk' = pkgs.callPackage naersk {
-          cargo = toolchain;
-          rustc = toolchain;
-        };
-
-      in {
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-        };
-
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ 
-            toolchain
-            rust-analyzer
-            bacon
-          ];
-        };
-      }
-    );
 }
