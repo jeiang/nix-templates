4e7905a650097b83f71a54b290b2bd750f08e3af
Aidan Pinard <aidan@aidanpinard.co>
2023-04-24 21:39:43 -0400

    added zig template

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         path = ./rust-naersk;
         description = "Flake for a Rust App using Naersk";
       };
+      zig = {
+        path = ./zig;
+        descriptions = "Flake for a Zig program using the latest version of zig";
+      };
     };
   };
 }
